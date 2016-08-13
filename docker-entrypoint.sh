21c392a6aef25bbcbb399fe40332f25a14029c34
wanghongxu <wang19930902@gmail.com>
2016-08-13 13:12:17 +0800

    fix bug for run error
    error: file not found

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 function defaults {
     : ${PORT=8000}
-    : ${DATADIR='~/packages'}
+    : ${DATADIR='/root/packages'}
 
     echo "PORT is ${PORT}"
     echo "DATADIR is ${DATADIR}"
@@ -14,7 +14,7 @@ defaults
 
 if [ "$1" = 'pypiserver' ]; then
     echo "[RUN]: Launching pypiserver"
-    exec pypi-server -p ${PORT} -P ~/htpasswd.txt ${DATADIR}
+    exec pypi-server -p ${PORT} -P /root/htpasswd.txt ${DATADIR}
 fi
 
 echo "[RUN]: Builtin command not provided [pypiserver]"
